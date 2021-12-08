d7fd52bb250ddc08815e66f261e1b70a7d0bb027
Rhea M <rheamalviya@rheas-air-2.myfiosgateway.com>
2021-12-07 22:13:06 -0500

    added confirm prompts and generatedpassword constant

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,51 @@ var generateBtn = document.querySelector("#generate");
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upper = lower.map.toUpperCase
 var number = [0,1,2,3,4,5,6,7,8,9]
-var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'", "+", ",", "-", ".", "/", ":", ";", "<", "=", "[", "]", "\", "_", "{", "}", "|", "~""]
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // password variable values are arrays
 //confirm option variables
 //password length is also defined
+var length =""
 //
+function generatePassword () {
+  const genpassword =[]
+  console.log(genpassword)
+  if (generateBtn) {
+    length=(prompt("How long do you want your password to be? You must pick a number between 8 and 128."));
+
+    if (isNaN(length)) {
+      alert("You must input a numeric amount between 8 and 128.")
+      generatePassword()
+    }
+    if(length<8 || length>128) {
+      alert("Your numeric input must be between 8 and 128.")
+      generatePassword()
+    }
+    if (8<=length<=128) {
+      // length must determine length of pw, store somewhere
+      window.confirm("Would you like lowercase letters?")
+
+      if(confirm) {
+        genpassword.push(lower)
+      }
+      window.confirm("Would you like uppercase letters?")
+
+      if(confirm) {
+        genpassword.push(upper)
+      }
+      window.confirm("Would you like numbers?")
+
+      if(confirm) {
+        genpassword.push(number)
+      }
+      window.confirm("Would you like special characters?")
+
+      if(confirm) {
+        genpassword.push(character)
+      }
+    }
+}
+};
 // if button is clicked then start password generator prompts
 //password generator prompts launched by generatepassword function
 // define empty array var choices = UI values
