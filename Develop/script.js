e8aff550446c0ed85b2716e3c6230dc71f5502fa
Rhea M <rheamalviya@rheas-air-2.myfiosgateway.com>
2021-12-09 12:46:12 -0500

    parsed length string and added for loop and confirmpassword pushes, also crated randomized arrays to push into password

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,69 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
  //establish global objects
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upper = lower.map.toUpperCase
+var upper = lower.map(lower => lower.toUpperCase())
 var number = [0,1,2,3,4,5,6,7,8,9]
 var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-// password variable values are arrays
-//confirm option variables
+  //generate randomized values for arrays?
+var rmLower= lower[(Math.random()*lower.length)|0]
+var rmUpper= upper[(Math.random()*upper.length)|0]
+var rmNumber= number[(Math.random()*number.length)|0]
+var rmCharacter= character[(Math.random()*character.length)|0]
 //password length is also defined
 var length =""
 //
 function generatePassword () {
+// define empty array var choices = UI values
   const genpassword =[]
   console.log(genpassword)
   if (generateBtn) {
-    length=(prompt("How long do you want your password to be? You must pick a number between 8 and 128."));
+    length= parseInt(prompt("How long do you want your password to be? You must pick a number between 8 and 128."));
 
     if (isNaN(length)) {
       alert("You must input a numeric amount between 8 and 128.")
       generatePassword()
     }
-    if(length<8 || length>128) {
+    else if(length<8 || length>128) {
       alert("Your numeric input must be between 8 and 128.")
       generatePassword()
     }
-    if (8<=length<=128) {
-      // length must determine length of pw, store somewhere
-      window.confirm("Would you like lowercase letters?")
-
-      if(confirm) {
-        genpassword.push(lower)
+    else (8<=length<=128);
+      // length must determine length of pw, store somewhere (in for loop?)
+      //confirm option variables
+      confirmLower= confirm("Would you like lowercase letters?")
+      confirmUpper= confirm("Would you like uppercase letters?")
+      confirmNumber= confirm("Would you like numbers?")
+      confirmCharacter=confirm("Would you like special characters?")
+    };
+  // if password length is estabilished then continue to next prompts
+      if(confirmLower) {
+        genpassword.push(rmLower)
       }
-      window.confirm("Would you like uppercase letters?")
 
-      if(confirm) {
-        genpassword.push(upper)
+      if(confirmUpper) {
+        genpassword.push(rmUpper)
       }
-      window.confirm("Would you like numbers?")
 
-      if(confirm) {
-        genpassword.push(number)
+      if(confirmNumber) {
+        genpassword.push(rmNumber)
       }
-      window.confirm("Would you like special characters?")
 
-      if(confirm) {
-        genpassword.push(character)
+      if(confirmCharacter) {
+        genpassword.push(rmCharacter)
       }
-    }
-}
-};
-// if button is clicked then start password generator prompts
-//password generator prompts launched by generatepassword function
-// define empty array var choices = UI values
-// var password (defined globally) = output in the html element
-//start length prompt
-// if it satisifies condition between 8 and 128 then estabilish length of password
-// else return to beginning of function (call function again)
-// if password length is estabilished then continue to next prompts
-// window.confirm Upper
-// if okay
-//if confirmUpper
-//choices.push()
-//push array Upper into password
-// if not
-// if!confirmUpper then move on
-// window.confirm Lower
-// if okay
-// if not
-// window.confirm Number
-// if okay
-// if not
-// window.confirm Character
-// if okay
-// if not
 // take choices and put them in for loop
 // math.floor/math.random
 // take results of for loop and push into password array
+ for (var i=0; i<length; i++) {
+
+ }
+};
+
+
+
 // Write password to the #password input
 function writePassword() {
+    //turn the result into string ?
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
